f613b0e4c3528e7c88270b01eabfd40c162c5e00
Saifuddin <saifuddin3pixel@gmail.com>
2026-07-05 17:46:22 +0530

    ..

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,70 +1,136 @@
 const Appointment = require("../models/Appointment");
-const getAppointments = async (req, res) => {
+  const getAppointments = async (req, res) => {
 
-  try {
+    try {
 
-    const appointments = await Appointment.find().sort({
-      createdAt: -1,
-    });
-
-    res.json({
-      success: true,
-      count: appointments.length,
-      data: appointments,
-    });
-
-  } catch (err) {
-
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-
-  }
-
-};
-
-// Create Appointment
-const createAppointment = async (req, res) => {
-  try {
-    const { name, email, phone, date, time, message } = req.body;
-
-    if (!name || !email || !phone || !date) {
-      return res.status(400).json({
-        success: false,
-        message: "Please fill all required fields",
+      const appointments = await Appointment.find().sort({
+        createdAt: -1,
       });
+
+      res.json({
+        success: true,
+        count: appointments.length,
+        data: appointments,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+
     }
 
-    const appointment = await Appointment.create({
-      name,
-      email,
-      phone,
-      date,
-      time,
-      message,
-    });
+  };
 
-    res.status(201).json({
-      success: true,
-      message: "Appointment Booked Successfully",
-      data: appointment,
-    });
+  // Create Appointment
+  const createAppointment = async (req, res) => {
+    try {
+      const { name, email, phone, date, time, message } = req.body;
 
-  } catch (err) {
+      if (!name || !email || !phone || !date) {
+        return res.status(400).json({
+          success: false,
+          message: "Please fill all required fields",
+        });
+      }
 
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
+      const appointment = await Appointment.create({
+        name,
+        email,
+        phone,
+        date,
+        time,
+        message,
+      });
 
-  }
-};
+      res.status(201).json({
+        success: true,
+        message: "Appointment Booked Successfully",
+        data: appointment,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+
+    }
+  };
+
+  // ==========================
+  // Delete Appointment
+  // ==========================
+  const deleteAppointment = async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const deletedAppointment = await Appointment.findByIdAndDelete(id);
+
+      if (!deletedAppointment) {
+        return res.status(404).json({
+          success: false,
+          message: "Appointment not found",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Appointment deleted successfully",
+      });
+
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
+  // ==========================
+  // Update Appointment
+  // ==========================
+  const updateAppointment = async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const updatedAppointment = await Appointment.findByIdAndUpdate(
+        id,
+        req.body,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+
+      if (!updatedAppointment) {
+        return res.status(404).json({
+          success: false,
+          message: "Appointment not found",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Appointment updated successfully",
+        data: updatedAppointment,
+      });
+
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
 
 module.exports = {
-    createAppointment,
-    getAppointments
-
+  createAppointment,
+  getAppointments,
+  deleteAppointment,
+  updateAppointment
 };
 // Get All Appointments
 
--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  createAppointment,
-  getAppointments,
+const { createAppointment,  getAppointments, deleteAppointment, updateAppointment
 } = require("../controllers/appointmentController");
 
-router.post("/", createAppointment);
-router.get("/", getAppointments);
+  router.post("/", createAppointment);
+  router.get("/", getAppointments);
+  // Update Appointment
+  router.put("/:id", updateAppointment);
+
+  // Delete Appointment
+  router.delete("/:id", deleteAppointment);
 
 module.exports = router;
